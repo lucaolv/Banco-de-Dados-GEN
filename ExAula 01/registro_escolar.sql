8ec52098a14b5d22fab9c27c4ce6ade3e0a4fa5c
Lucas Vinicius <111596218+lucaolv@users.noreply.github.com>
2023-06-16 22:54:42 -0300

    registro_escolar.sql

--- ExAula 01/registro_escolar.sql
+++ ExAula 01/registro_escolar.sql
@@ -44,3 +44,11 @@ SELECT aluno FROM estudantes WHERE nota > 7.0;
 SELECT aluno FROM estudantes WHERE nota < 7.0;
  
 UPDATE estudantes SET nota = 6.0 WHERE id = 4;
+
+--Crie um banco de dados para um registro de uma escola, onde o sistema trabalhará com as informações dos estudantes deste registro dessa escola. 
+--Crie uma tabela estudantes e utilizando a habilidade de abstração e determine 5 atributos relevantes dos estudantes para se trabalhar com o serviço dessa escola.
+--Insira nesta tabela no mínimo 8 dados (registros).
+--Faça um SELECT que retorne todes o/a(s) estudantes com a nota maior do que 7.0.
+--Faça um SELECT que retorne todes o/a(s) estudantes com a nota menor do que 7.0.
+--Ao término atualize um registro desta tabela através de uma query de atualização.
+--Salve todas as queries para cada um dos requisitos do exercício em um único script (arquivo .SQL) e coloque no seu Github pessoal, no repositório que você criou sobre Banco de dados.
